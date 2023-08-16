db79a8c5cfd4ec0b6546650712626376bb75058c
taiga <ice.taiga517@icloud.com>
2023-08-16 00:11:18 +0000

    Add a Static Pages controller

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  root "application#hello"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
