2417128beaa7882f834389fe23f72f4f288c1d58
taiga <ice.taiga517@icloud.com>
2023-08-24 08:09:17 +0000

    AWS S3

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 # exit on error
-set -e
+#set -e
+#set -o errexit
+#bundle install
+#bundle exec rails assets:precompile
+#bundle exec rails assets:clean
+#bundle exec rails db:migrate
 
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
-
+bundle exec rails db:migrate:reset
+bundle exec rails db:seed
