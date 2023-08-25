b269c6895ccefb8a1c370034e94edd27c8d69497
taiga <ice.taiga517@icloud.com>
2023-08-25 05:46:45 +0000

    finish sample_app

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
-#set -e
-#set -o errexit
-#bundle install
-#bundle exec rails assets:precompile
-#bundle exec rails assets:clean
-#bundle exec rails db:migrate
-
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate:reset
-bundle exec rails db:seed
+bundle exec rails db:migrate
